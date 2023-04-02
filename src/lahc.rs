cd712f11ae85eb984cbe99393fb80cacd78e21ff
aiwavision <aiwavision@protonmail.com>
2023-04-01 21:30:38 -0300

    latest changes

--- src/lahc.rs
+++ src/lahc.rs
@@ -1,17 +1,22 @@
 use std::collections::VecDeque;
 
 use crate::operations;
+use std::time::Instant;
 
 pub fn lahc(solution: &mut Vec<i32>, max_size: usize) -> i32 {
     let mut s = operations::count_perfect_squares(solution);
     let mut s_star = s;
     let mut f = VecDeque::from(vec![s; max_size]);
     let mut i = 0;
-    while i < 1000 {
-        if i % 100 == 0 {
-            println!("Iteration {}", i);
-        }
+    let mut i_idle = 0;
+    let instant = Instant::now();
+    while i_idle < 100 && instant.elapsed().as_secs_f64() < (10.0 * 60.0) {
         let s_linha = operations::first_improvement_solution(solution);
+        if s_linha <= s {
+            i_idle += 1;
+        } else {
+            i_idle = 0;
+        }
         if s_linha > f[0] || s_linha > s {
             s = s_linha;
         }
@@ -24,5 +29,10 @@ pub fn lahc(solution: &mut Vec<i32>, max_size: usize) -> i32 {
         }
         i += 1;
     }
+    println!(
+        "Finished at {} iterations and {:?} seconds",
+        i,
+        instant.elapsed().as_secs_f64()
+    );
     return s_star;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::time::Instant;
-
 pub mod files;
 pub mod lahc;
 pub mod operations;
@@ -13,10 +11,7 @@ fn main() {
     for (name, sol) in instances.iter() {
         println!("{}", name);
         let mut s = sol.clone();
-        let now = Instant::now();
-        let x = lahc::lahc(&mut s, 100);
-        println!("Best solution found is {}.", x);
-        let elapsed_time = now.elapsed();
-        println!("Took {} seconds.", elapsed_time.as_secs());
+        let x = lahc::lahc(&mut s, 50000);
+        println!("Best solution found is {}.\n", x);
     }
 }
